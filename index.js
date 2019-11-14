723cd601c277f5d1f80e77d8b9a4378d1d8c51fc
Rashdan Razak <rashdanrazak91@gmail.com>
2019-11-14 18:26:38 +0800

    [update]
    complete

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 //body parser
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
